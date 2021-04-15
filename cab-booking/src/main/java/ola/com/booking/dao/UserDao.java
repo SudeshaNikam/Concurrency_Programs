2af8db3aa61cbba97b6c544372df159301e25dcb
SudeshaNikam <77058369+SudeshaNikam@users.noreply.github.com>
2021-04-15 10:16:32 +0530

    commit

--- cab-booking/src/main/java/ola/com/booking/dao/UserDao.java
+++ cab-booking/src/main/java/ola/com/booking/dao/UserDao.java
@@ -7,6 +7,6 @@ import ola.com.booking.model.User;
 public interface UserDao {
 
 	void saveUser(User user);
-    User validateUser(String username, String password);
-	List<User> getUserData();
+       boolean validateUser(String username, String password);
+	List<User> getUserData(String uname);
 }
